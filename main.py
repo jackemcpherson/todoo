8b585b53f27b08ee5acba6b2c7588c5d3a19ba54
Jack McPherson <jackemcpherson@gmail.com>
2022-04-02 14:12:28 +1100

    added initial sqlalchemy code

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from tkinter.tix import COLUMN
 from fastapi import FastAPI, status
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+
+# Create a sqlite engine instance
+engine = create_engine("sqlite:///todooo.db")
+
+# Create a DeclarativeMeta instance
+Base = declarative_base()
+
+# Define To Do class inheriting from Base
+class ToDo(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer, primary_key=True)
+    task = Column(String(256))
+
+# Create the database
+Base.metadata.create_all(engine)
+
+# Initialize app
 app = FastAPI()
 
 @app.get("/")
