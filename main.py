8850ecf7ba348dd01610c049f965d3cb7640f581
Jack McPherson <jackemcpherson@gmail.com>
2022-04-02 14:46:37 +1100

    Expanded read_todo and read_todo_list functions.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
@@ -24,25 +24,44 @@ def root():
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 def create_todo(todo: ToDoRequest):
+
     # create a new database session
     session = Session(bind=engine, expire_on_commit=False)
 
     # create an instance of the ToDo database model
     tododb = ToDo(task=todo.task)
 
+    # add it to the session and commit it
     session.add(tododb)
     session.commit()
 
+    # grab the id given to the object from the database
     id = tododb.id
 
+    # close the session
     session.close()
 
+    # return the id
     return f"created todo item with id {id}"
 
 
 @app.get("/todo/{id}")
 def read_todo(id: int):
-    return "read todo item with id {id}"
+
+    # create a new database session
+    session = Session(bind=engine, expire_on_commit=False)
+
+    # get the todo item with the given id
+    todo = session.query(ToDo).get(id)
+
+    # close the session
+    session.close()
+
+    # check if todo item with given id exists. If not, raise exception and return 404 not found response
+    if not todo:
+        raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
+
+    return todo
 
 
 @app.put("/todo/{id}")
@@ -57,4 +76,13 @@ def delete_todo(id: int):
 
 @app.get("/todo")
 def read_todo_list():
-    return "read todo list"
+    # create a new database session
+    session = Session(bind=engine, expire_on_commit=False)
+
+    # get all todo items
+    todo_list = session.query(ToDo).all()
+
+    # close the session
+    session.close()
+
+    return todo_list
