4e92f69adf087225bdddf785e2ee526d5374c3c0
Jack McPherson <jackemcpherson@gmail.com>
2022-04-02 15:45:33 +1100

    Shed formatting

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from typing import List
-from fastapi import Depends, FastAPI, status, HTTPException
-from database import Base, engine, SessionLocal
+
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
+
 import models
 import schemas
+from database import Base, SessionLocal, engine
 
 # Create the database
 Base.metadata.create_all(engine)
 
 # Initialize app
 app = FastAPI()
+
 
 def get_session():
     session = SessionLocal()
@@ -18,15 +21,17 @@ def get_session():
     finally:
         session.close()
 
+
 @app.get("/")
 def root():
     return "todooo"
+
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session)):
 
     # create an instance of the ToDo database model
-    tododb = models.ToDo(task = todo.task)
+    tododb = models.ToDo(task=todo.task)
 
     # add it to the session and commit it
     session.add(tododb)
@@ -35,6 +40,7 @@ def create_todo(todo: schemas.ToDoCreate, session: Session = Depends(get_session
 
     # return the id
     return tododb
+
 
 @app.get("/todo/{id}", response_model=schemas.ToDo)
 def read_todo(id: int, session: Session = Depends(get_session)):
@@ -48,8 +54,9 @@ def read_todo(id: int, session: Session = Depends(get_session)):
 
     return todo
 
+
 @app.put("/todo/{id}")
-def update_todo(id: int, task: str,  session: Session = Depends(get_session)):
+def update_todo(id: int, task: str, session: Session = Depends(get_session)):
 
     # get the todo item with the given id
     todo = session.query(models.ToDo).get(id)
@@ -65,8 +72,9 @@ def update_todo(id: int, task: str,  session: Session = Depends(get_session)):
 
     return todo
 
+
 @app.delete("/todo/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_todo(id: int,  session: Session = Depends(get_session)):
+def delete_todo(id: int, session: Session = Depends(get_session)):
 
     # get the todo item with the given id
     todo = session.query(models.ToDo).get(id)
@@ -79,6 +87,7 @@ def delete_todo(id: int,  session: Session = Depends(get_session)):
         raise HTTPException(status_code=404, detail=f"todo item with id {id} not found")
 
     return None
+
 
 @app.get("/todo", response_model=List[schemas.ToDo])
 def read_todo_list(session: Session = Depends(get_session)):
--- models.py
+++ models.py
@@ -1,8 +1,10 @@
 from sqlalchemy import Column, Integer, String
+
 from database import Base
+
 
 # Define To Do class inheriting from Base
 class ToDo(Base):
-    __tablename__ = 'todos'
+    __tablename__ = "todos"
     id = Column(Integer, primary_key=True)
     task = Column(String(256))
