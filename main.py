92ca9e88faacac503a1b2cf9c74593eb347b719d
Jack McPherson <jackemcpherson@gmail.com>
2022-04-02 14:14:55 +1100

    refactor + shed formatting

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from tkinter.tix import COLUMN
 from fastapi import FastAPI, status
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 
-
-# Create a sqlite engine instance
-engine = create_engine("sqlite:///todooo.db")
-
-# Create a DeclarativeMeta instance
-Base = declarative_base()
-
-# Define To Do class inheriting from Base
-class ToDo(Base):
-    __tablename__ = 'todos'
-    id = Column(Integer, primary_key=True)
-    task = Column(String(256))
+from database import Base, engine
 
 # Create the database
 Base.metadata.create_all(engine)
@@ -22,25 +8,31 @@ Base.metadata.create_all(engine)
 # Initialize app
 app = FastAPI()
 
+
 @app.get("/")
 def root():
     return "todooo"
+
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
 def create_todo():
     return "create todo item"
 
+
 @app.get("/todo/{id}")
 def read_todo(id: int):
     return "read todo item with id {id}"
+
 
 @app.put("/todo/{id}")
 def update_todo(id: int):
     return "update todo item with id {id}"
 
+
 @app.delete("/todo/{id}")
 def delete_todo(id: int):
     return "delete todo item with id {id}"
+
 
 @app.get("/todo")
 def read_todo_list():
